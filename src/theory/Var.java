3a157f53eb7880c5dd94a4de8fa3d3fc81c6c11b
rodrigocmo <47004786+rodrigocmo@users.noreply.github.com>
2024-01-16 19:20:53 -0300

    more var infos

--- src/theory/Var.java
+++ src/theory/Var.java
@@ -3,10 +3,12 @@ package theory;
 public class Var {
 
     public void var(){
-        var hello = "string";
+        var var = "string";
 
         // var is only allowed for local variables
         // you can't use var word for classes but remember java is case-sensitive
+        // var can't be null with the type is primitive
+        // var can't be used to multiple assign like var name =1, name2 =2;
     }
 
 }
