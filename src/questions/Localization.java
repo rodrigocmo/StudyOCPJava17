9ce76db470acb0edec89e8a5d8b53fd4ff8bb7ed
rodrigocmo <47004786+rodrigocmo@users.noreply.github.com>
2024-07-06 15:44:11 -0300

    feat: localization + resourceBundle

--- src/questions/Localization.java
+++ src/questions/Localization.java
@@ -1,8 +1,13 @@
 package questions;
 
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class Localization {
 
@@ -10,6 +15,38 @@ public class Localization {
     public static void main(String[] args) {
 
 
+
+    }
+    void resourceBundle(){
+
+        //Zoo_pt_BR.properties exemplo de resourceBundle
+        ResourceBundle rb = ResourceBundle.getBundle("arquivoaprocurar", new Locale("pt","br"));
+        rb.getString("exemploDeKey");
+        rb.keySet().stream().forEach(System.out::println);
+
+        //Ordem -> Vai procurar pelo nome que voce passou, depois nome + linguagem , default, default + linguagem, so nome arquivo ou da MissingResourceException
+
+        //Ordem se achar -> procura no que achou , depois nome + linguagem e por fim só nome
+
+        Properties prop = new Properties();
+        prop.setProperty("oi","teste");
+        prop.setProperty("oi2","teste2");
+
+        String testeString= "Hello,{0} and {1}";//exemplo de chave valor no resource
+
+        System.out.println(MessageFormat.format(testeString,prop.getProperty("oi","notfound"),prop.get("oi2")));
+
+    }
+
+    void compact(){
+        NumberFormat nb = NumberFormat.getCompactNumberInstance(new Locale("en"), NumberFormat.Style.LONG);
+        System.out.println(nb.format(1000000.00));// 1 milhao
+
+        NumberFormat n = NumberFormat.getCompactNumberInstance(new Locale("en"), NumberFormat.Style.SHORT);
+        System.out.println(n.format(1000000.00));
+
+        //mostra até 3 numeros e arredonda o ultimo
+        System.out.println(n.format(357.99));
     }
     void instances(){
 
